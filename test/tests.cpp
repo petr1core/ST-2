8e8b28b6851905343e469f566ce6d4563da328f9
thunderbird <greg.varfolomeev@yandex.ru>
2025-03-11 19:15:52 +0300

    s

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,10 @@
 // Copyright 2025 UNN-CS Team
-#include "circle.h"
-#include "tasks.h"
 #include <cmath>
 #include <cstdint>
 #include <gtest/gtest.h>
+#include "tasks.h"
+#include "circle.h"
+
 
 #define _USE_MATH_DEFINES
 
